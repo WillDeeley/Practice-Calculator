cf22ba7c936b8c55b0227ce0afddf973c1935be9
william-deeley <william.deeley@capgemini.com>
2017-07-11 12:14:19 +0100

    Added loop functionality for ability to do another calculation with calc.

--- src/Calcu/Calc.java
+++ src/Calcu/Calc.java
@@ -9,45 +9,50 @@ public class Calc {
     static double num1;
     static double num2;
     static double ans;
+    static String again = "";
 
     public static void main(String[] args) {
 
         do {
-            System.out.println("1 for Addition. 2 for Subtraction. 3 for Multiplication. 4 for Division");
-            op = scan.nextInt();
-        } while (op<1 && op>4);
-
-        if (op==1){
-            System.out.println("Input your first number");
-            num1 = scan.nextInt();
-            System.out.println("Input your second number");
-            num2 = scan.nextInt();
-            ans = num1 + num2;
-            System.out.println("Your answer is " +ans);
-        }
-        if (op==2){
-            System.out.println("Input your first number");
-            num1 = scan.nextInt();
-            System.out.println("Input your second number");
-            num2 = scan.nextInt();
-            ans = num1 - num2;
-            System.out.println("Your answer is " +ans);
-        }
-        if (op==3){
-            System.out.println("Input your first number");
-            num1 = scan.nextInt();
-            System.out.println("Input your second number");
-            num2 = scan.nextInt();
-            ans = num1 * num2;
-            System.out.println("Your answer is " +ans);
-        }
-        if (op==4){
-            System.out.println("Input your first number");
-            num1 = scan.nextInt();
-            System.out.println("Input your second number");
-            num2 = scan.nextInt();
-            ans = num1 / num2;
-            System.out.println("Your answer is " +ans);
-        }
+            do {
+                System.out.println("1 for Addition. 2 for Subtraction. 3 for Multiplication. 4 for Division");
+                op = scan.nextInt();
+            } while (op < 1 || op > 4);
+            if (op == 1) {
+                System.out.println("Input your first number");
+                num1 = scan.nextInt();
+                System.out.println("Input your second number");
+                num2 = scan.nextInt();
+                ans = num1 + num2;
+                System.out.println("Your answer is " + ans);
+            }
+            if (op == 2) {
+                System.out.println("Input your first number");
+                num1 = scan.nextInt();
+                System.out.println("Input your second number");
+                num2 = scan.nextInt();
+                ans = num1 - num2;
+                System.out.println("Your answer is " + ans);
+            }
+            if (op == 3) {
+                System.out.println("Input your first number");
+                num1 = scan.nextInt();
+                System.out.println("Input your second number");
+                num2 = scan.nextInt();
+                ans = num1 * num2;
+                System.out.println("Your answer is " + ans);
+            }
+            if (op == 4) {
+                System.out.println("Input your first number");
+                num1 = scan.nextInt();
+                System.out.println("Input your second number");
+                num2 = scan.nextInt();
+                ans = num1 / num2;
+                System.out.println("Your answer is " + ans);
+            }
+            scan.nextLine();
+            System.out.println("Again? Y/N");
+            again = scan.nextLine();
+        }while(again.toUpperCase().equals("Y"));
     }
 }
